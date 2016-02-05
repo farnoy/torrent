8a8c9ac83b60dd209bd952a6c93c265f9020e276
Jakub Okoński <jakub@okonski.org>
2016-02-05 22:45:05 +0100

    Use QuickCheck for FileWriter specs.

--- test/FileWriterSpec.hs
+++ test/FileWriterSpec.hs
@@ -4,38 +4,43 @@
 module FileWriterSpec where
 
 import Control.Concurrent.MVar (newMVar)
-import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Lazy.Char8 as BLC
+import Data.Char (chr)
 import qualified Data.Sequence as Seq
 import Data.Word
 import qualified Network.BitTorrent.FileWriter as FW
-import Network.BitTorrent.Utility
 import System.Directory
 import System.IO
 
-import SpecHelper
 import Test.Hspec
-import Test.Hspec.SmallCheck
-import Test.SmallCheck
-import Test.SmallCheck.Series
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
+import Test.QuickCheck.Instances ()
 
 data RangeFile = RangeFile Word32 FilePath deriving(Show)
 
-instance Monad m => Serial m RangeFile where
-  series = cons2 RangeFile
+readableChar :: Gen Word8
+readableChar = choose (48, 122)
+
+readableGen :: Gen String
+readableGen = listOf1 (chr . fromIntegral <$> readableChar)
+
+instance Arbitrary RangeFile where
+  arbitrary = RangeFile <$> choose (1, 8192) <*> readableGen
+  shrink (RangeFile len path) = (`RangeFile` path) <$> shrinkIntegral len
 
 parseRangeFiles :: [RangeFile] -> (Word32, [(Word32, Word32, FilePath)])
 parseRangeFiles = foldl folder (0, [])
   where folder (offset, list) (RangeFile size path) =
           (offset + size, list ++ [(offset, offset + size, path)])
 
-openFiles :: [(Word32, Word32, FilePath)] -> BL.ByteString -> IO [(BL.ByteString, (Word32, Word32, Handle))]
-openFiles files filler = getTemporaryDirectory >>= \dir -> traverse (opener dir) files
+openFiles :: [(Word32, Word32, FilePath)] -> IO [(BL.ByteString, (Word32, Word32, Handle))]
+openFiles files = getTemporaryDirectory >>= \dir -> traverse (opener dir) files
   where opener dir (lo, hi, _) = do
           (path, handle) <- openTempFile dir "test"
           removeFile path
-          let content = BL.take (fromIntegral $ hi - lo) $ BL.drop (fromIntegral lo) filler
+          let content = BL.take (fromIntegral $ hi - lo) $ BL.drop (fromIntegral lo) testString
           BL.hPut handle content
           return (content, (lo, hi, handle))
 
@@ -46,24 +51,34 @@ spec :: SpecWith ()
 spec = do
   describe "FileWriter" $ do
     describe "read" $ do
-      it "property test" $ property $ \rangeFiles -> monadic $ do
+      prop "property test" $ \rangeFiles ->
         let (totalSize, parsedFiles) = parseRangeFiles rangeFiles
-        lock <- newMVar ()
-        openedFiles <- openFiles parsedFiles testString
-        return $ forAll $ \(lo, hi) -> lo < hi && hi < totalSize ==> monadic $ do
-          dataRead <- FW.read (Seq.fromList $ snd <$> openedFiles) lock lo (hi - lo)
-          let actual = BL.toStrict $ BL.take (fromIntegral $ hi - lo) $ BL.drop (fromIntegral lo) testString
-          return $ dataRead == actual
+        in totalSize > 0 ==> ioProperty $ do
+          lock <- newMVar ()
+          openedFiles <- openFiles parsedFiles
+          let generator = do
+                lo <- choose (0, totalSize - 1)
+                hi <- choose (lo, totalSize)
+                return (lo, hi)
+          return $ forAll generator $ \(lo, hi) -> ioProperty $ do
+            dataRead <- FW.read (Seq.fromList $ snd <$> openedFiles) lock lo (hi - lo)
+            let actual = BL.toStrict $ BL.take (fromIntegral $ hi - lo) $ BL.drop (fromIntegral lo) testString
+            return $ dataRead === actual
 
     describe "write" $ do
-      it "property test" $ property $ \rangeFiles -> monadic $ do
+      prop "property test" $ \rangeFiles ->
         let (totalSize, parsedFiles) = parseRangeFiles rangeFiles
-        lock <- newMVar ()
-        openedFiles <- openFiles parsedFiles testString
-        return $ forAll $ \(lo, hi) -> lo < hi && hi < totalSize ==>
-          forAll $ \toWrite -> (fromIntegral $ B.length toWrite) + lo == hi ==> monadic $ do
+        in totalSize > 0 ==> ioProperty $ do
+          lock <- newMVar ()
+          openedFiles <- openFiles parsedFiles
+          let generator = do
+                str <- infiniteListOf readableChar
+                lo <- choose (0, totalSize - 1)
+                hi <- choose (lo, totalSize)
+                return (lo, hi, BL.toStrict $ BL.take (fromIntegral $ hi - lo) $ BL.pack str)
+
+          return $ forAll generator $ \(lo, hi, toWrite) -> ioProperty $ do
             let hdls = Seq.fromList $ snd <$> openedFiles
             FW.write hdls lock lo toWrite
             readBack <- FW.read hdls lock lo (hi - lo)
-            return $ toWrite == readBack
-
+            return $ toWrite === readBack
