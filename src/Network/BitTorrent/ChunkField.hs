e647ed1ba6d75cf55f2b35b2a52e039f4acd984f
Jakub Okoński <jakub@okonski.org>
2016-02-14 21:10:50 +0100

    Don't store completedChunks in ChunkField.

--- src/Network/BitTorrent/ChunkField.hs
+++ src/Network/BitTorrent/ChunkField.hs
@@ -32,14 +32,12 @@ import Network.BitTorrent.Utility
 data ChunkField = ChunkField
   { missingChunks :: IntSet -- ^ Missing chunks
   , requestedChunks :: IntSet -- ^ Requested chunks
-  , completedChunks :: IntSet -- ^ Completed chunks
   } deriving(Eq, Show, Generic, NFData)
 
 -- | Creates a new 'ChunkField' with specified length and all
 -- chunks marked as missing.
 newChunkField :: Int -> ChunkField
 newChunkField n = ChunkField (IntSet.fromList [0..(n-1)])
-                              IntSet.empty
                               IntSet.empty
 {-# INLINABLE newChunkField #-}
 
@@ -49,7 +47,7 @@ newChunkField n = ChunkField (IntSet.fromList [0..(n-1)])
 -- 'ChunkField' and its ID.
 getNextChunk :: ChunkField
              -> Maybe (ChunkField, ChunkId)
-getNextChunk cf@(ChunkField missing requested _) = go (IntSet.null missing)
+getNextChunk cf@(ChunkField missing requested) = go (IntSet.null missing)
   where go False = let (k, missing') = IntSet.deleteFindMin missing
                    in Just (cf { missingChunks = missing'
                                , requestedChunks = IntSet.insert k requested }
@@ -59,40 +57,37 @@ getNextChunk cf@(ChunkField missing requested _) = go (IntSet.null missing)
 
 -- | Mark the chunk as completed.
 markCompleted :: ChunkField -> ChunkId -> ChunkField
-markCompleted cf@(ChunkField missing requested completed) (ChunkId ix) =
+markCompleted cf@(ChunkField missing requested) (ChunkId ix) =
   cf { requestedChunks = IntSet.delete id requested
-     , missingChunks = IntSet.delete id missing
-     , completedChunks = IntSet.insert id completed }
+     , missingChunks = IntSet.delete id missing }
   where id = fromIntegral ix
 {-# INLINABLE markCompleted #-}
 
 -- | Mark the chunk as requested.
 markRequested :: ChunkField -> ChunkId -> ChunkField
-markRequested cf@(ChunkField missing requested completed) (ChunkId ix) =
+markRequested cf@(ChunkField missing requested) (ChunkId ix) =
   cf { missingChunks = IntSet.delete id missing
-     , completedChunks = IntSet.delete id completed
      , requestedChunks = IntSet.insert id requested }
   where id = fromIntegral ix
 {-# INLINABLE markRequested #-}
 
 -- | Mark the chunk as missing.
 markMissing :: ChunkField -> ChunkId -> ChunkField
-markMissing cf@(ChunkField missing requested completed) (ChunkId ix) =
+markMissing cf@(ChunkField missing requested) (ChunkId ix) =
   cf { missingChunks = IntSet.insert id missing
-     , requestedChunks = IntSet.delete id requested
-     , completedChunks = IntSet.delete id completed }
+     , requestedChunks = IntSet.delete id requested }
   where id = fromIntegral ix
 {-# INLINABLE markMissing #-}
 
 -- | /O(1)/ Check if the 'ChunkField' is completed.
 -- To be completed, it cannot have any missing or requested pieces.
 isCompleted :: ChunkField -> Bool
-isCompleted (ChunkField missing requested _) =
+isCompleted (ChunkField missing requested) =
   IntSet.null missing && IntSet.null requested
 {-# INLINABLE isCompleted #-}
 
 -- | /O(1)/ Check if the 'ChunkField' is requested.
 -- To be requested, it cannot have any missing pieces.
 isRequested :: ChunkField -> Bool
-isRequested (ChunkField missing _ _) = IntSet.null missing
+isRequested (ChunkField missing _) = IntSet.null missing
 {-# INLINABLE isRequested #-}
